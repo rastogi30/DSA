6286c8862abe9f0d6765fae0f19722b82d31c6a5
rastogi30 <lakhanrastogi2002@gmail.com>
2025-06-27 19:19:27 +0530

    Time: 856 ms (9.42%), Space: 10.7 MB (96.38%) - LeetHub

---
+++ 2140-longest-subsequence-repeated-k-times/2140-longest-subsequence-repeated-k-times.cpp
@@ -0,0 +1,77 @@
+class Solution {
+public:
+    // check subsequence h ya nhi
+    bool isSubsequence(string &s, string &sub, int k){
+       int i=0;
+       int j=0;
+       int l=sub.length();
+       int n=s.length();
+
+       while(i<n && j<k*l){
+        // string k time create nhi kari size se mode kr ke check kr rahe...
+        // pr condition k*l length ki
+        if(s[i]==sub[j%l]){
+            j++;
+        }
+        i++;
+       }
+       // id j==k*l means j reached end so subsequence h..return true;  
+       return j==k*l;
+    }
+
+    void backtracking(string& s, string& curr, vector<bool>& canUse, vector<int>& freqReq, int k,
+    int maxLen, string& result){
+        if(curr.length()>maxLen){
+            return;
+        }
+        // curr string ko update kr diya
+        if(((curr.length()>result.length()) || 
+           (curr.length()==result.length() && curr>result)) && isSubsequence(s, curr, k)){
+             result=curr;
+        }
+
+        for(int i=0;i<26;i++){
+            if(canUse[i] == false || freqReq[i]==0){
+                continue;
+            }
+
+            char ch=i+'a';
+            curr.push_back(ch);
+            freqReq[i]--;
+
+            backtracking(s, curr, canUse, freqReq, k, maxLen, result);
+
+            curr.pop_back();
+            freqReq[i]++;
+        }
+    }
+
+    string longestSubsequenceRepeatedK(string s, int k) {
+        int n=s.length();
+        string result;
+
+        // maxLength of the sequence
+        int maxLen=n/k;
+
+        int freq[26]={};
+        for(int i=0;i<n;i++){
+            freq[s[i]-'a']++;
+        }
+
+        vector<bool>canUse(26,false);
+        vector<int>freqReq(26,0);
+        for(int i=0;i<26;i++){
+            if(freq[i]>=k){
+                canUse[i]=true;
+                freqReq[i]=freq[i]/k; // atmost this can be used in one sequence
+            }
+        }
+
+        string curr;
+        backtracking(s, curr, canUse, freqReq, k, maxLen, result);
+        return result;
+    }
+};
+
+// TC: O(n* (n/k)!)
+// space O(n/k) at most hold strings of size O(n/k) -> very small.
