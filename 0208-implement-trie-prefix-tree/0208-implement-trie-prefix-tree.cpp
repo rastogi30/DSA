5a21d5f3a8e766d2c239beb7102a0a01345a3296
rastogi30 <lakhanrastogi2002@gmail.com>
2025-08-18 23:21:14 +0530

    Time: 87 ms (6.1%), Space: 142.3 MB (5.08%) - LeetHub

---
+++ 0208-implement-trie-prefix-tree/0208-implement-trie-prefix-tree.cpp
@@ -0,0 +1,110 @@
+class TrieNode{
+    public:
+    char data;
+    TrieNode* children[26];
+    bool isTerminal;
+
+    TrieNode(char ch){
+        data=ch;
+        for(int i=0;i<26;i++){
+            children[i]=NULL;
+        }
+        isTerminal=false;
+    }
+};
+
+class Trie {
+    TrieNode* root;
+public:
+    Trie() {
+        root= new TrieNode('\0');
+    }
+    
+    void insertUtil(TrieNode*root, string word){
+        // base case
+        if(word.length()==0){
+           root->isTerminal=true;
+           return;
+        }
+
+        int index=word[0]-'a';
+        TrieNode* child;
+
+        if(root->children[index]!=NULL){
+            // present
+            child=root->children[index];
+        }
+        else{
+            // absent
+            child= new TrieNode(word[0]);
+            root->children[index]=child;
+        }
+        // recursion
+        insertUtil(child, word.substr(1));
+    }
+
+    void insert(string word) {
+        insertUtil(root, word);
+    }
+
+    bool searchUtil(TrieNode*root, string word){
+        // base case
+        if(word.length()==0){
+            return root->isTerminal;
+        }
+
+        int index=word[0]-'a';
+        TrieNode* child;
+
+        if(root->children[index]!=NULL){
+            // present
+            child=root->children[index];
+        }
+        else{
+            // absent
+            return false;
+        }
+        // recursion
+       return searchUtil(child, word.substr(1));
+    }
+    bool search(string word) {
+        return searchUtil(root, word);
+    }
+    
+    bool startsWithUtil(TrieNode*root, string word){
+        // base case
+        if(word.length()==0){
+            return true;
+        }
+
+        int index=word[0]-'a';
+        TrieNode* child;
+
+        if(root->children[index]!=NULL){
+            // present
+            child=root->children[index];
+        }
+        else{
+            // absent
+            return false;
+        }
+        // recursion
+        return startsWithUtil(child, word.substr(1));
+    }
+    bool startsWith(string prefix) {
+        return startsWithUtil(root, prefix);
+    }
+};
+
+// Insert: O(L)
+// Search: O(L)
+// StartsWith: O(L)
+// we pass substr instead pass the index and in each do index++;
+
+/**
+ * Your Trie object will be instantiated and called as such:
+ * Trie* obj = new Trie();
+ * obj->insert(word);
+ * bool param_2 = obj->search(word);
+ * bool param_3 = obj->startsWith(prefix);
+ */
